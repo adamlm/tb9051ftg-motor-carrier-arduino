2d7ab04de022d9e33b997eb0164663b56d6bb16a
Adam Morrissett <me@adamlm.com>
2020-06-20 11:31:09 -0400

    Fix bug for throttle percent
    - Change thorttle percent to float
    - Increment amount by 0.1 instead of 10

--- examples/simple_control/simple_control.ino
+++ examples/simple_control/simple_control.ino
@@ -7,7 +7,7 @@ static constexpr uint8_t pwm2Pin{6};
 // Instantiate TB9051FTG
 static const TB9051FTG driver{pwm1Pin, pwm2Pin};
 
-static uint8_t throttlePercent{0};
+static float throttlePercent{0.0f};
 
 void setup() {
   driver.enable();
@@ -18,28 +18,28 @@ void loop() {
     // Ramp up to full forward throttle
     for (auto i{0}; i < 10; i++) {
         driver.setOutput(throttlePercent);
-        throttlePercent += 10;
+        throttlePercent += 0.1;
         delay(1000);
     }
 
     // Ramp down to 0 throttle
     for (auto i{0}; i < 10; i++) {
         driver.setOutput(throttlePercent);
-        throttlePercent -= 10;
+        throttlePercent -= 0.1;
         delay(1000);
     }
 
     // Ramp up to full reverse throttle
     for (auto i{0}; i < 10; i++) {
         driver.setOutput(throttlePercent);
-        throttlePercent -= 10;
+        throttlePercent -= 0.1;
         delay(1000);
     }
 
     // Ramp up to 0 throttle
     for (auto i{0}; i < 10; i++) {
         driver.setOutput(throttlePercent);
-        throttlePercent += 10;
+        throttlePercent += 0.1;
         delay(1000);
     }
 }
